9e1b7819718da4238dd9d0255cc0521a4ca7b50e
wate <pc.poisoning@gmail.com>
2017-12-17 22:40:15 +0900

    とりあえずDebian用に書き直し

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
-describe 'role mackerel' do
-  describe yumrepo('mackerel') do
-    it { should exist }
-    it { should be_enabled }
-  end
-  describe package('mackerel-agent') do
-    it { should be_installed }
-  end
+describe yumrepo('mackerel') do
+  it { should exist }
+  it { should be_enabled }
+end
+describe package('mackerel-agent') do
+  it { should be_installed }
 end
